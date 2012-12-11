76c5fa53aecd7868c9c392d17fbf45cf6d002b33
Bryant Luk <bryant.luk@bryantluk.com>
2012-12-10 18:39:26 -0600

    Initial Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeatherWidget::Application.config.secret_token = '8f97c8826b76cd0c69e25f04133c14a757d6a96b870a5a704b65ea4f9efee4ce589760abdd1d51747e4aeadb1d36b6d240f217922be3caed2b1b2b893cbde213'
