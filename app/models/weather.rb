4d7961d5ab4fc9663bc8fc24325beaeb4aa02beb
Bryant Luk <bryant.luk@bryantluk.com>
2012-12-10 20:22:31 -0600

    Adds Weather model to_s for debugging

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -1,3 +1,7 @@
 class Weather
-  attr_accessor :current_temp_f, :wind_speed, :id
+  attr_accessor :current_temp_f, :wind_speed, :id, :city, :state
+
+  def to_s
+    "#{id} / #{city}, #{state} - #{current_temp_f} degrees - #{wind_speed} wind speed"
+  end
 end
