9c1c0f5cd9fbb5b1a1a69e70ccc74edc8416b584
Bryant Luk <bryant.luk@bryantluk.com>
2013-01-22 21:58:25 -0600

    Adds current_temp_f and wind_speed to WeatherPlaces

--- app/controllers/weather_places_controller.rb
+++ app/controllers/weather_places_controller.rb
@@ -3,8 +3,17 @@ class WeatherPlacesController < ApplicationController
   def index
     @weather_places = WeatherPlace.all
 
+    @weather_places.each do |p|
+      if @is_mock
+        get_mock_weather_for(p)
+      else
+        get_weather_for(p)
+      end
+      puts "after loop #{p.current_temp_f}"
+    end
+
     respond_to do |format|
-      format.json { render json: @weather_places }
+      format.json { render json: @weather_places, methods: [:current_temp_f, :wind_speed] }
     end
   end
 
@@ -12,8 +21,14 @@ class WeatherPlacesController < ApplicationController
   def show
     @weather_place = WeatherPlace.find(params[:id])
 
+    if @is_mock
+      get_mock_weather_for(@weather_place)
+    else
+      get_weather_for(@weather_place)
+    end
+
     respond_to do |format|
-      format.json { render json: @weather_place }
+      format.json { render json: @weather_place, methods: [:current_temp_f, :wind_speed] }
     end
   end
 
@@ -52,4 +67,42 @@ class WeatherPlacesController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+  protected
+    def get_weather_for(p)
+      w_api = Wunderground.new(ENV['WUNDERGROUND_API_KEY'])
+      data = nil
+
+      if p.state && p.city
+        data = w_api.conditions_for(URI::encode(p.state), URI::encode(p.city))
+        p.zipcode = "#{p.city}, #{p.state}".to_zip.first
+      elsif p.zipcode
+        data = w_api.conditions_for(p.zipcode)
+        p.city = p.zipcode.to_region(:city => true)
+        p.state = p.zipcode.to_region(:state => true)
+      end
+
+      if data != nil
+        observation = data['current_observation']
+        if observation != nil
+          p.current_temp_f = observation['temp_f']
+          p.wind_speed = observation['wind_mph']
+          puts "Silly temp: #{p.current_temp_f}"
+        end
+      end
+
+      p
+    end
+
+    def get_mock_weather_for(p)
+      p.current_temp_f = rand(120) + (rand(2) == 0 ? rand().round(1) : 0)
+      p.wind_speed = rand(30) + (rand(2) == 0 ? rand().round(1) : 0)
+      p
+    end
+
+    def check_if_mock
+      if ENV['MOCK_WEATHER']
+        @is_mock = true
+      end
+    end
 end
--- app/models/weather_place.rb
+++ app/models/weather_place.rb
@@ -1,3 +1,5 @@
 class WeatherPlace < ActiveRecord::Base
-  attr_accessible :city, :country, :state, :zipcode
+  attr_accessor :current_temp_f, :wind_speed
+
+  attr_accessible :city, :country, :state, :zipcode, :current_temp_f, :wind_speed
 end
